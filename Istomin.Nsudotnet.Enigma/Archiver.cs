ecef2e4ab7abbf75eb4a4d366d26522c5fba69b4
Istomin Mikhail <mistler.nsu@gmail.com>
2016-05-12 21:22:05 +0500

    Enigma: initial code created, no algorithms yet
    Main created, args parsing created, algorithms not implemented.

---
+++ Istomin.Nsudotnet.Enigma/Archiver.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Istomin.Nsudotnet.Enigma
+{
+    internal enum Algorithm
+    {
+        AES,
+        DEC,
+        RC2,
+        Rijndael
+    }
+
+    class Archiver
+    {
+        private static Dictionary<string, Algorithm> formatsDictionary = new Dictionary<string, Algorithm>
+        {
+            {"AES", Algorithm.AES},
+            {"DEC", Algorithm.DEC},
+            {"RC2", Algorithm.RC2},
+            {"Rijndael", Algorithm.Rijndael}
+        };
+
+        private Algorithm algorithm;
+
+        public Archiver(string algorithm)
+        {
+            try
+            {
+                this.algorithm = formatsDictionary[algorithm];
+            }
+            catch (KeyNotFoundException ex)
+            {
+                throw new ArgumentException("Not supported algorithm: " + algorithm);
+            }
+
+        }
+
+        public void Encode(string input, string output)
+        {
+            using (FileStream inputStream = File.Open(input, FileMode.Open, FileAccess.Read),
+                outputStream = File.Open(output, FileMode.Open, FileAccess.Write))
+            {
+                switch (algorithm)
+                {
+                    case Algorithm.AES:
+                    case Algorithm.DEC:
+                    case Algorithm.RC2:
+                    case Algorithm.Rijndael:
+                        throw new NotImplementedException("Sorry, but currently this algorithm is not implemented!");
+                }
+            }
+        }
+
+        public void Decode(string input, string output, string key)
+        {
+            using (FileStream inputStream = File.Open(input, FileMode.Open, FileAccess.Read),
+                outputStream = File.Open(output, FileMode.Open, FileAccess.Write),
+                keyStream = File.Open(key, FileMode.Open, FileAccess.Read))
+            {
+                switch (algorithm)
+                {
+                    case Algorithm.AES:
+                    case Algorithm.DEC:
+                    case Algorithm.RC2:
+                    case Algorithm.Rijndael:
+                        throw new NotImplementedException("Sorry, but currently this algorithm is not implemented!");
+                }
+            }
+        }
+    }
+}
---
+++ Istomin.Nsudotnet.Enigma/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Istomin.Nsudotnet.Enigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 4)   // Encryption.
+            {
+                string direction = args[0];
+                string input = args[1];
+                string algorithm = args[2];
+                string output = args[3];
+
+                if (direction == "encrypt")
+                {
+                    Archiver archiver = new Archiver(algorithm);
+                    archiver.Encode(input, output);
+                    return;
+                }
+            }
+            else if (args.Length == 5)   // Decryption.
+            {
+                string direction = args[0];
+                string input = args[1];
+                string algorithm = args[2];
+                string key = args[3];
+                string output = args[4];
+
+                if (direction == "decrypt")
+                {
+                    Archiver archiver = new Archiver(algorithm);
+                    archiver.Decode(input, output, key);
+                    return;
+                }
+            }
+
+            System.Console.WriteLine("Incorrect argument count!");
+            System.Console.WriteLine("Usage:");
+            System.Console.WriteLine("\tEncryption: crypto.exe encrypt <input> <algorithm> <output>");
+            System.Console.WriteLine("\tDecryption: crypto.exe decrypt <input> <algorithm> <key_file> <output>");
+        }
+    }
+}
